314bed7d74ca2071592bcb80afa31b7ff5fefdd8
Phuong To <tophu95@gmail.com>
2019-05-02 16:49:59 +0100

    positioning assets

--- flappyBird.js
+++ flappyBird.js
@@ -12,10 +12,10 @@ var pipeNorth = new Image();
 var pipeSouth = new Image();
 
 pidgeot.src = "images/pidgeot.png";
-bg.src = "images/bg.png";
-fg.src = "images/fg.png";
-pipeNorth.src = "images/pipeNorth.png";
-pipeSouth.src = "images/pipeSouth.png";
+bg.src = "images/bg-wide-edit.png";
+fg.src = "images/fg2.png";
+pipeNorth.src = "images/pipeNorth2.png";
+pipeSouth.src = "images/pipeSouth2.png";
 
 // Load Audios
 
@@ -23,24 +23,43 @@ pipeSouth.src = "images/pipeSouth.png";
 // audioName.src = "audio/fly.mp3";
 
 // Const Variable
-var gap = 75;
+var gap = 85;
 var constant = pipeNorth.height + gap
 
 var bX = 10;
 var bY = 150;
+var gravity = 1;
+
+// Event Listener Key Down
+
+document.addEventListener("keydown", moveUp);
+function moveUp() {
+  bY -= 20;
+}
+
+// Pipe coordinates
+
+var pipe = [];
+pipe[0] = {
+  x: cvs.clientWidth,
+  y: 0
+}
 
 // Draw Images -- Including, Bird, Position, Wight & Height
-
 function draw() {
   // Placing BG image on Canvas
   cxt.drawImage(bg, 0, 0);
+  for (var i = 0; i < pipe.length; i++) {
+    cxt.drawImage(pipeNorth, pipe[i].x, pipe[i].y);
+    cxt.drawImage(pipeSouth, pipe[i].x, pipe[i].y + constant);
+    pipe[i].x--;
+  }
 
-  cxt.drawImage(pipeNorth, 100, 0);
-  cxt.drawImage(pipeSouth, 100, 0 + constant);
-
-  cxt.drawImage(fg, 0, cvs.height - fg.height);
+  // cxt.drawImage(fg, 0, cvs.height - fg.height);
 
   cxt.drawImage(pidgeot, bX, bY);
+
+  bY += gravity;
 
   requestAnimationFrame(draw);
 
